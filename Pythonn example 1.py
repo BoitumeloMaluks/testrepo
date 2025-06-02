acef4df0d7793d1ed60d7a0429817c1a1bd0ee6b
Boitumelo Maluleke <143790158+BoitumeloMaluks@users.noreply.github.com>
2025-06-02 12:12:17 +0200

    Added files via upload

---
+++ Pythonn example 1.py
@@ -0,0 +1,6 @@
+import math
+radius = int(input("Radius of circle: "))
+area = math.pi * radius**2 
+#print("The area of a circle with radius",radius," is equal to", area )
+print("r =",radius)
+print("Area = ",area)
